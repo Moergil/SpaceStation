e4ed8eebfe42ddf2402b392c01008e55c612d36c
Matej Isteník <matej.istenik@gmail.com>
2014-08-23 09:11:38 -0700

    Initial commit

---
+++ desktop/src/sk/hackcraft/spacestation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sk.hackcraft.spacestation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sk.hackcraft.spacestation.SpaceStationGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceStationGame(), config);
+	}
+}

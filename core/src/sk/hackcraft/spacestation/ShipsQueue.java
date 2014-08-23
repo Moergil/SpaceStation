9399f29fe2722034bc5c69a87ff67920133390f0
Matej Isteník <matej.istenik@gmail.com>
2014-08-23 11:17:31 -0700

    Added clean.xml Added ship and game views basics

---
+++ core/src/sk/hackcraft/spacestation/ShipsQueue.java
@@ -0,0 +1,30 @@
+package sk.hackcraft.spacestation;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+
+public class ShipsQueue extends Actor
+{
+	public ShipsQueue()
+	{
+
+	}
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha)
+	{
+		// TODO Auto-generated method stub
+		super.draw(batch, parentAlpha);
+	}
+	
+	@Override
+	public void act(float delta)
+	{
+		// TODO Auto-generated method stub
+		super.act(delta);
+	}
+	
+	
+}

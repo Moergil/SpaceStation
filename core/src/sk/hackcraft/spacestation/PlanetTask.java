8b94d5c1cda552eff89748205cfb257137716dc7
ElisModra <matahlinkova@gmail.com>
2014-08-24 10:01:38 +0200

    fdfdf

--- core/src/sk/hackcraft/spacestation/PlanetTask.java
+++ core/src/sk/hackcraft/spacestation/PlanetTask.java
@@ -1,6 +1,30 @@
 package sk.hackcraft.spacestation;
 
-public class PlanetTask
-{
+import java.util.Date;
 
+public class PlanetTask implements Comparable
+{
+	private GoodsType type;
+	private int amount;
+	private Date fromTime;
+	private Date toTime;
+	
+
+	public PlanetTask(GoodsType type, int amount, Date fromTime, Date toTime)
+	{
+		this.type = type;
+		this.amount = amount;
+		this.fromTime = fromTime;
+		this.toTime = toTime;
+	}
+
+
+	@Override
+	public int compareTo(Object arg0)
+	{
+		return this.toTime.compareTo(toTime);
+	}
+	
+	
+	
 }

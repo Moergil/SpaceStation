95f9bf0b0004161689a0badcdce7ce2c54a2b1e0
Matej Isteník <matej.istenik@gmail.com>
2014-08-24 08:46:17 -0700

    Added interface for interacting with Station View

---
+++ core/src/sk/hackcraft/spacestation/StationView.java
@@ -0,0 +1,18 @@
+package sk.hackcraft.spacestation;
+
+/**
+ * Interface for interacting view view to station. It allows to add ship to view
+ * (so it will be visible on the waiting ships queue) and register callback for
+ * undocked ship.
+ */
+public interface StationView
+{
+	void shipArrived(Ship ship);
+
+	void setListener();
+
+	public interface StationViewListener
+	{
+		void shipUndocked(Ship ship);
+	}
+}
